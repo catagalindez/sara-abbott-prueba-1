ee310a3dad680729f56749d78a20c2e2eab785d4
TrioloValentina <valentinatriolo22@gmail.com>
2023-06-07 16:25:48 -0300

    nuevo

--- sketch.js
+++ sketch.js
@@ -14,11 +14,30 @@ function setup () {
 }
 
 function draw(){
-    background(220,220,220)
+    background(220,220,220,220)
+
+    let xoff = frameCount * 0.01;
+      for( let i=0 ; i<width ; i++ ){
+        let yoff = 0.0;
+        xoff += 0.01;    
+
+      for( let j=0 ; j<height ; j++ ){
+        yoff += 0.01;
+    //let valor = random(0 , height );
+        let valor = noise( xoff, yoff );
+
+        let elColor = lerpColor( color(255,0,0) , color(0,0,255), valor);
+
+        stroke( elColor );
+        point(i,j);
+       }
+    }
 
     let cual = int( random(cantidad));
     let x = random( width );
     let y = random( height );
     
     image( arreglo[cual] , x, y);
+
+    
 }
